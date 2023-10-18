42b7a2e68ecf9682f0a674432d160fd0cc6049d3
Igor <chumikov.igor@mail.ru>
2023-10-18 18:08:09 +0700

    pod install

---
+++ Pods/Target Support Files/Pods-SwiftMocking-SwiftMockingUITests/Pods-SwiftMocking-SwiftMockingUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftMocking_SwiftMockingUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftMocking_SwiftMockingUITests
+@end
---
+++ Pods/Target Support Files/Pods-SwiftMocking/Pods-SwiftMocking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftMocking : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftMocking
+@end
