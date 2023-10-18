87a626eeb90c1452e0426b88dba197adc5a4bc5d
Igor <chumikov.igor@mail.ru>
2023-10-18 17:55:13 +0700

    Add SwiftMocking

---
+++ SwiftMocking/SwiftMockingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMockingApp.swift
+//  SwiftMocking
+//
+//  Created by Игорь Чумиков on 18.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMockingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
