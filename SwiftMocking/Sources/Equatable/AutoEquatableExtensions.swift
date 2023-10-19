cedf3ad33291a6d7dade448345e9c6089e2f3ae9
Igor <chumikov.igor@mail.ru>
2023-10-19 21:16:12 +0700

    Fix

---
+++ SwiftMocking/Sources/Equatable/AutoEquatableExtensions.swift
@@ -0,0 +1,11 @@
+//
+//  AutoEquatableExtensions.swift
+//  SwiftMocking
+//
+//  Created by Игорь Чумиков on 19.10.2023.
+//
+// sourcery:file: AutoEquatable
+
+import Foundation
+
+extension PostingActionButtonsStyle {}
---
+++ SwiftMocking/Sources/GeneratedContent/AutoHashable.generated.swift
@@ -0,0 +1,8 @@
+// Generated using Sourcery 2.1.1 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+// swiftlint:disable all
+
+
+// MARK: - AutoHashable for classes, protocols, structs
+
+// MARK: - AutoHashable for Enums
